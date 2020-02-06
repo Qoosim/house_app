53aad447b92c4c7f625f8180a074d150f911dd87
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-02-06 18:28:51 +0100

    Update for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  root 'houses/index'
+
   resources :houses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
